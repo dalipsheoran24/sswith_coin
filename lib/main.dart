cedcf5693e2354eb671c22b5efaab5807b227aac
dalipsheoran24 <dlsheoran.199@gmail.com>
2021-05-20 21:22:51 +0530

    implemented coinswitch

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-//import 'dart:html';
 
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -110,9 +110,108 @@ class Homepage extends StatelessWidget {
               ),
               shape: BoxShape.rectangle,
             ),
-          )
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Text(('Popular Currencies'),
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+
+                  ),
+                ),
+              )
+            ],
+          ),
+         Expanded(
+           child: ListView(
+             children: [
+              columnInfo("Bitcoin","BTC", "RS 3345533", "-33.54%"),
+               columnInfo("Ethereum","ETH", "RS 224729", "-21.54%"),
+               columnInfo("Waves","WAVES", "RS 2229", "+16.4%"),
+               columnInfo("Digbyte","DGB", "RS 7.54", "-7.51%"),
+               columnInfo("Dotecoin","DOGE", "RS 32.54", "+2.45%"),
+               columnInfo("Terra","LUNA", "RS 791.84", "-33.76%"),
+               columnInfo("Tezos","XTZ", "RS 337.49", "+1.73%"),
+               columnInfo("Elrond","EGLD", "RS 9517.56", "-4.77%"),
+
+             ],
+           ),
+         )
         ],
       ),
     );
+  }
+
+  Column columnInfo(String coin,String name, String rate,String percent) {
+    return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+
+                        )),
+
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon (Icons.info_sharp),
+                        ),
+                        Column(
+                          children: [
+                            Text(coin ,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w900,
+                            ),
+                            ),
+                            Text(name,
+                            style: TextStyle(
+                              fontSize: 12,
+
+                            ),
+                            ),
+                          ],
+                        ),
+                        Expanded(child: Container()),
+                        Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(100, 0, 0, 0),
+
+                              child: Text(rate,
+                                style: TextStyle(
+                                  fontSize: 20,
+
+                                ),
+
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(150, 0, 0, 0),
+                              child: Text(percent,
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.red,
+                              ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            );
   }
 }
